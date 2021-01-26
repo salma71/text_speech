c3fe4f85787445fcfa53cdbfb3a21346b951b992
Salma Elshahawy <salmaeng71@gmail.com>
2021-01-26 18:51:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,28 +18,4 @@ iface = gr.Interface(fn = pdf_to_text,
                             <p> All you need to do is to upload the pdf file and hit submit, then wait for compiling. After that click on Play/Pause for listing to the audio. The audio is saved in a wav format.</p>
                         </div>'''
                     )
-iface.launch(share = True)
-        
-# iface.launch()
-# from pydub import AudioSegment
-# import pyttsx3
-# import PyPDF2
-# import gradio as gr
-
-# import subprocess
-
-   
-# def pdf_to_text(file_obj):
-#     speaker = pyttsx3.init()
-#     pdfReader = PyPDF2.PdfFileReader(open(file_obj.name, 'rb'))
-#     pages = pdfReader.numPages
-#     speaker.say(pages)
-#     speaker.runAndWait()
-#     speaker.save_to_file(pages, '/Users/salmaelshahawy/Desktop/text_speech/ss')
-#     AudioSegment.from_file('/Users/salmaelshahawy/Desktop/text_speech/ss').export('converted.wav', format="wav")
-#     return 'converted.wav'
-
-# iface = gr.Interface(fn = pdf_to_text, inputs = 'file', outputs = 'audio')
-        
-# iface.launch()
-# # pdf_to_text('ESLII.pdf')
+iface.launch()
